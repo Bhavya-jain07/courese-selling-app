f88079f9b523937b3226607759d7fe75d6ccb71d
Bhavya-jain07 <jain1983.nisha@gmail.com>
2026-05-19 17:27:01 +0530

    added admin Router

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 
 const {userRouter} =  require("./routes/user.routes")
 const {courseRouter} = require("./routes/course.routes")
-
+const adminRouter = require ("./routes/admin.routes")
 
 app.use("/user",userRouter);
+app.use("/admin", adminRouter);
 app.use("/course", courseRouter);
 
 
